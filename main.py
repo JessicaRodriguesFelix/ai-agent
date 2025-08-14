3e6539e1ab1af4bb3b609d225f80a2f990869889
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2025-08-14 10:39:47 -0400

    pull different llama model, adds propmpot template and creates agent

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_ollama import OllamaLLM
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
-load_dotenv()
+load_dotenv() # might not be required anymore since we are using llma
 
 # set up an LLM
-llm = OllamaLLM(model='llama3.2:1b')
-response = llm.invoke("What is the capital of France?")
-print(response)
+llm = ChatOllama(model='llama3.1:8b')
+# response = llm.invoke("What is the capital of France?")
+# print(response)
+
+# setup a prompt template
+class ResearchResponseModel(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+# parser takes the output from the llm and parses it into the model created, this can be later used as a python object inside the code
+parser = PydanticOutputParser(pydantic_object=ResearchResponseModel)
+
+prompt = ChatPromptTemplate.from_messages([
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]).partial(format_instructions=parser.get_format_instructions())
+
+
+agent = create_tool_calling_agent(llm=llm, prompt=prompt, tools=[])
+
+agent_executor = AgentExecutor(agent=agent, tools=[],verbose=True)
+# run the agent executor with a input
+raw_response = agent_executor.invoke({"input": "What is the capital of France?"})
+print(raw_response)
